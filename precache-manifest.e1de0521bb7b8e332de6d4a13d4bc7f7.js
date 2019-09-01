dc0a4d7104ee4d207f5e7cc97e7e095d545edec0
George Luke <george.luke92@gmail.com>
2019-08-31 19:17:02 -0500

    Updates

---
+++ precache-manifest.e1de0521bb7b8e332de6d4a13d4bc7f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b5da2486ad754dbbfcc77206c3641e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05c9f5985dab55047ea4",
+    "url": "/monsters-rolodex/static/css/main.9d2ccf0b.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "05c9f5985dab55047ea4",
+    "url": "/monsters-rolodex/static/js/main.4eccbcc3.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
